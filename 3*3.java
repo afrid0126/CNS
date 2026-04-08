bcbe2f41ed6fca00be693f81eb8088a35c73c637
afrid0126 <afridk2601@gmail.com>
2026-04-08 19:51:14 +0530

    Refactor HillCipher into Main class and adjust logic

--- 3*3.java
+++ 3*3.java
@@ -1,41 +1,35 @@
 import java.util.*;
 
-class HillCipher {
-    static int[][] key = {
-        {6, 24, 1},
-        {13, 16, 10},
-        {20, 17, 15}
-    };
-
-    static int[] multiply(int[][] key, int[] pt) {
-        int[] result = new int[3];
-
-        for (int i = 0; i < 3; i++) {
-            result[i] = 0;
-            for (int j = 0; j < 3; j++) {
-                result[i] += key[i][j] * pt[j];
-            }
-            result[i] %= 26;
-        }
-        return result;
-    }
-
+class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter 3-letter plaintext: ");
-        String text = sc.next().toUpperCase();
+        // 3x3 key matrix
+        int[][] key = {
+            {6, 24, 1},
+            {13, 16, 10},
+            {20, 17, 15}
+        };
 
-        int[] pt = new int[3];
-        for (int i = 0; i < 3; i++) {
-            pt[i] = text.charAt(i) - 'A';
+        String msg = "HIIAMAFRID";
+
+        // Make length multiple of 3 (padding with X if needed)
+        while (msg.length() % 3 != 0) {
+            msg += "X";
         }
 
-        int[] ct = multiply(key, pt);
+        for (int i = 0; i < msg.length(); i += 3) {
 
-        System.out.print("Ciphertext: ");
-        for (int i = 0; i < 3; i++) {
-            System.out.print((char)(ct[i] + 'A'));
+            int a = msg.charAt(i) - 'A';
+            int b = msg.charAt(i + 1) - 'A';
+            int c = msg.charAt(i + 2) - 'A';
+
+            int c1 = (key[0][0]*a + key[0][1]*b + key[0][2]*c) % 26;
+            int c2 = (key[1][0]*a + key[1][1]*b + key[1][2]*c) % 26;
+            int c3 = (key[2][0]*a + key[2][1]*b + key[2][2]*c) % 26;
+
+            System.out.print((char)(c1 + 'A'));
+            System.out.print((char)(c2 + 'A'));
+            System.out.print((char)(c3 + 'A'));
         }
     }
-              }
+}
